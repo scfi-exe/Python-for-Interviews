3294ea0e2be2a84cf15362e03bc213ba5975bf5d
scfi-exe <scottfinkelstein97@gmail.com>
2025-09-12 19:40:48 -0400

    Sorted Set Basics

--- Python-for-Interviews/08. Sorted Dicts and Sorted Sets/01.5. Sorted Set Basics.py
+++ Python-for-Interviews/08. Sorted Dicts and Sorted Sets/01.5. Sorted Set Basics.py
@@ -12,3 +12,40 @@ my_set.add(90)
 my_set.add(80)
 my_set.add(70)
 print(my_set)  # SortedSet([70, 80, 90])
+
+
+# DELETION: Delete a key from the sorted set
+
+my_set = SortedSet([90, 80, 85, 95])
+
+my_set.remove(90)  # SortedSet([80, 85, 95])
+
+my_set.discard(100)  # SortedSet([80, 85, 95])
+
+my_set.pop()  # 95
+
+my_set.pop(0)  # 80
+
+print(my_set)  # SortedSet([85])
+
+my_set.clear()  # SortedSet([])
+
+# the set.remove(key) method will raise a KeyError if the element does not exist, while the set.discard(key) method will not
+# the set.pop() method will remove and return the largest element in the sorted set
+# the set.pop(0) method will remove and return the smallest element in the sorted set
+# the set.clear() method will remove all elements from the sorted set
+
+
+# LOOKUP: Check if a key exists in the sorted set
+my_set = SortedSet([80, 85, 90])
+
+does_a_exist = "a" in my_set  # True
+does_b_exist = "b" in my_set  # False
+
+
+# ITERATING: Loop through a sorted set
+
+sorted_set = SortedSet([4, 3, 5, 2, 1])
+
+for num in sorted_set:
+    print(num)  # 1, 2, 3, 4, 5
